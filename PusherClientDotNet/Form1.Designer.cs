0900528a8688fcf62382645b849610eaf8933807
Richard Wang <a2h@live.com.au>
2011-04-30 20:59:00 +1000

    Add a demo

--- PusherClientDotNet/Form1.Designer.cs
+++ PusherClientDotNet/Form1.Designer.cs
@@ -28,12 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxApiKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxChannel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxEvents = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBoxConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxConsole.Location = new System.Drawing.Point(14, 130);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBox1";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxConsole.Size = new System.Drawing.Size(555, 167);
+            this.textBoxConsole.TabIndex = 0;
+            this.textBoxConsole.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "API key:";
+            // 
+            // textBoxApiKey
+            // 
+            this.textBoxApiKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxApiKey.Location = new System.Drawing.Point(91, 14);
+            this.textBoxApiKey.Name = "textBoxApiKey";
+            this.textBoxApiKey.Size = new System.Drawing.Size(478, 23);
+            this.textBoxApiKey.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Channel:";
+            // 
+            // textBoxChannel
+            // 
+            this.textBoxChannel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxChannel.Location = new System.Drawing.Point(91, 43);
+            this.textBoxChannel.Name = "textBoxChannel";
+            this.textBoxChannel.Size = new System.Drawing.Size(478, 23);
+            this.textBoxChannel.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Events:";
+            // 
+            // textBoxEvents
+            // 
+            this.textBoxEvents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEvents.Location = new System.Drawing.Point(91, 72);
+            this.textBoxEvents.Name = "textBoxEvents";
+            this.textBoxEvents.Size = new System.Drawing.Size(478, 23);
+            this.textBoxEvents.TabIndex = 6;
+            // 
+            // buttonListen
+            // 
+            this.buttonConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConnect.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonConnect.Location = new System.Drawing.Point(482, 101);
+            this.buttonConnect.Name = "buttonListen";
+            this.buttonConnect.Size = new System.Drawing.Size(87, 23);
+            this.buttonConnect.TabIndex = 7;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonUnlisten
+            // 
+            this.buttonDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonDisconnect.Location = new System.Drawing.Point(389, 101);
+            this.buttonDisconnect.Name = "buttonUnlisten";
+            this.buttonDisconnect.Size = new System.Drawing.Size(87, 23);
+            this.buttonDisconnect.TabIndex = 8;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 312);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxEvents);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxChannel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxApiKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxConsole);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "PusherClientDotNet Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxApiKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxChannel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxEvents;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
     }
 }
 
--- PusherClientDotNet/Form1.cs
+++ PusherClientDotNet/Form1.cs
@@ -11,9 +11,52 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Pusher _pusher;
+        Pusher.Channel _pusherChannel;
+
         public Form1()
         {
             InitializeComponent();
+            Pusher.OnLog += new PusherLogHandler(Pusher_OnLog);
+        }
+
+        void Pusher_OnLog(object sender, PusherLogEventArgs e)
+        {
+            if (textBoxConsole.InvokeRequired)
+            {
+                textBoxConsole.Invoke((Action)(() => Pusher_OnLog(sender, e)));
+                return;
+            }
+
+            textBoxConsole.Text += e.Message;
+            foreach (object obj in e.Additional)
+            {
+                textBoxConsole.Text += " | ";
+                if (obj is Pusher.JsonData)
+                    textBoxConsole.Text += Pusher.JSON.stringify(obj);
+                else
+                    textBoxConsole.Text += obj == null ? "null" : obj.ToString();
+            }
+
+            textBoxConsole.Text += Environment.NewLine;
+            textBoxConsole.Select(textBoxConsole.Text.Length, 0);
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            _pusher = new Pusher(textBoxApiKey.Text);
+            _pusherChannel = _pusher.Subscribe(textBoxChannel.Text);
+            foreach (string channel in textBoxEvents.Text.Split(','))
+                _pusherChannel.Bind(channel.Trim(), d => MessageBox.Show(Pusher.JSON.stringify(d)));
+            buttonConnect.Enabled = false;
+            buttonDisconnect.Enabled = true;
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            _pusher.Disconnect();
+            buttonConnect.Enabled = true;
+            buttonDisconnect.Enabled = false;
         }
     }
 }
